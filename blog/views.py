74a251a358e4ae2bf6795291175baa05326732ef
albertgalicinskiy <galicinskiyalba@mail.ru>
2021-09-26 17:56:13 +0500

    Создали модель Блог и настроили вывод на странице /blog

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = Blog.objects.all()
+
+    # Отсортировать по дате и выдать последние свежие статьи из блога кол-ом 5шт
+    # blogs = Blog.objects.order_by('-date')[:5]
+
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
