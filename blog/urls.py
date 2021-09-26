485cfff59ab6d236b86b42f49d6c4ef25e460b8f
albertgalicinskiy <galicinskiyalba@mail.ru>
2021-09-26 21:29:52 +0500

    Добавили страинцу details

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
     # path('linux/', views.all_blogs, name='all_blogs'),
     # path('aster/', views.all_blogs, name='all_blogs'),
+
+    # '<int:blog_id>/' - сюда будет приходить запрос из чисел и будет искать id
+    # числа в БД через функцию detail в views.py
+    path('<int:blog_id>/', views.detail, name='detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -10,3 +10,9 @@ def all_blogs(request):
     # blogs = Blog.objects.order_by('-date')[:5]
 
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    # Вернем отдельный объект из БД, исп-я id из urls.py в перемнное block_id
+    # в противном случае будет ошибка 404 если нет номера с таким id
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
